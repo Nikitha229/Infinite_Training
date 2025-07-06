b765a160f49a367153a60e452973f0584f8a88b6
nikithap <nikithap@ICS-LT-DJ24D64>
2025-07-06 16:58:19 +0530

    Assignment_6

---
+++ CSharp/Assignments/Assignment_6/Assignment_6/Question_2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+    // Question 1 already dome in assignment 5
+
+    // Write a program in C# Sharp to create a file and write an array of strings to the file.
+
+    class Question_2
+    {
+        static void WriteStream(string[] lines)
+        {
+            FileStream fs = new FileStream("C:\\Infinite-Training\\CSharp\\Assignments\\Assignment_6\\Assignment_6\\Sample_Strings_Question_2_File.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (var str in lines)
+            {
+                sw.WriteLine(str);
+            }
+            sw.Flush();
+            sw.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            Console.Write("Enter number of strings: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("Enter string {0}", i+1);
+                string s = Console.ReadLine();
+                lines.Add(s);
+            }
+            WriteStream(lines.ToArray());
+            Console.WriteLine("Strings successfully added in the file");
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Assignments/Assignment_6/Assignment_6/Question_3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+    class Question_3
+    {
+        static void ReadStream()
+        {
+            FileStream fs = new FileStream("C:\\Infinite-Training\\CSharp\\Assignments\\Assignment_6\\Assignment_6\\Sample_Strings_Question_2_File.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            int count = 0;
+            string str = sr.ReadLine();
+            while (str != null)
+            {
+                count++;
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+            Console.WriteLine("Number of lines in file: {0}", count);
+            sr.Close();
+            fs.Close();
+        }
+        public static void Main()
+        {
+            ReadStream();
+            Console.Read();
+        }
+    }
+}
