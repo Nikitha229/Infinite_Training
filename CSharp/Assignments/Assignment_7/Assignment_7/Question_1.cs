28635e9e4f6cae847ab7de40c519528a499ebd93
nikithap <nikithap@ICS-LT-DJ24D64>
2025-07-06 18:00:23 +0530

    Assignment_7

---
+++ CSharp/Assignments/Assignment_7/Assignment_7/Question_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_7
+{
+    class Question_1
+    {
+        static void Main(string[] args)
+        {
+
+            List<int>nums=new List<int>();
+            Console.Write("Enter number of elements: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Elements: ");
+            for(int i=0;i<n;i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                nums.Add(a);
+            }
+            var res=nums.Where(x=>x*x>20).Select(x=>$"{x} - {x * x}");
+
+            foreach (var i in res)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Assignments/Assignment_7/Assignment_7/Question_4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment_7_Question_4;
+
+namespace Assignment_7
+{
+    class Question_4
+    {
+        public static void Main()
+        {
+            Console.Write("Enter your name: ");
+            string Name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int Age = int.Parse(Console.ReadLine());
+            Console.Write("Enter the total fare: ");
+            double TotalFare = double.Parse(Console.ReadLine());
+
+            Assignment_7_Question_4.TravelConcession travelConcession = new TravelConcession();
+            string result = travelConcession.CalculateConcession(Age, TotalFare);
+            Console.WriteLine($"Hello {Name}, {result}");
+            Console.Read();
+        }
+     }
+
+}
