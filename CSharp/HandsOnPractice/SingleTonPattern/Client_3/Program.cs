ea2984155a6dff19afc81e41d7b96737b6a70ab5
nikithap <nikithap@ICS-LT-DJ24D64>
2025-07-10 11:33:26 +0530

    SingleTonPatterns

---
+++ CSharp/HandsOnPractice/SingleTonPattern/Client_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingleTonPattern;
+namespace Client_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTonClass training = SingleTonClass.GetInstance();
+            {
+                training.Show("This is Client_3");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ CSharp/HandsOnPractice/SingleTonPattern/SingleTonPattern/SingleTonClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonPattern
+{
+    public class SingleTonClass
+    {
+        //private field to increment a counter every time object is created
+        private static int counter = 0;
+
+        //instance object to store the singleton instance
+        private static SingleTonClass spobj = null;
+
+        //public method that returns the singleton object
+        public static SingleTonClass GetInstance()
+        {
+            if (spobj == null)
+            {
+                spobj = new SingleTonClass();
+            }
+            return spobj;
+        }
+
+        //private constructor (parameter less)
+        private SingleTonClass()
+        {
+            counter++;
+            Console.WriteLine("Counter Value : " + counter.ToString());
+        }
+
+        //other normal instance methods
+        public void Show(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
